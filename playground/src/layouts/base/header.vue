b7686818eb3b1b8d324cd29edbea1412d30953bd
aibayanyu20 <aibayanyu@qq.com>
2026-01-06 13:46:18 +0800

    chore: change

--- playground/src/layouts/base/header.vue
+++ playground/src/layouts/base/header.vue
@@ -97,7 +97,7 @@ const localeValue = computed(() => {
     <a-row>
       <a-col :xxl="4" :xl="5" :lg="6" :md="6" :sm="24" :xs="24">
         <h1 class="m-0 p-0">
-          <router-link class="inline-flex items-center pl-40px h-[var(--ant-doc-header-height)] line-height-[var(--ant-doc-header-height)] text-18px font-bold a-color-text hover:a-color-text of-hidden" to="/">
+          <router-link class="inline-flex items-center pl-40px h-(--ant-doc-header-height) line-height-[var(--ant-doc-header-height)] text-18px font-bold a-color-text hover:a-color-text of-hidden" to="/">
             <img src="../../assets/antdv-next.svg" class="w-36px h-36px inline-block align-middle" draggable="false" alt="logo">
             <span class="ml-2">
               Antdv Next
@@ -106,7 +106,7 @@ const localeValue = computed(() => {
         </h1>
       </a-col>
       <a-col :xxl="20" :xl="19" :lg="18" :md="18" :sm="0" :xs="0">
-        <div class="ant-doc-header-right flex items-center pr-[var(--ant-padding)] gap-sm h-full">
+        <div class="ant-doc-header-right flex items-center pr-(--ant-padding) gap-sm h-full">
           <div class="b-l-1 b-l-solid b-l-black/6 flex items-center m-0" style="flex: auto">
             <SearchIcon class="ant-doc-search-bar-svg" />
             <input v-model="searchValue" class="ant-doc-search-bar-input" placeholder="输入关键字搜索...">
